87bedede2337f0a535ee007a98bcb0952bf60528
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-05-20 10:57:17 +0200

    topology documentation

--- src/main/java/model/Topology.java
+++ src/main/java/model/Topology.java
@@ -1,31 +1,58 @@
 package model;
 
 import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
 
-import java.util.ArrayList;
 
+/**
+ * the topology object
+ * this class is to encapsulate topology data in one object
+ * @author Ahmed Abdallah
+ */
 public class Topology {
+    // the id of the topology
     String id;
+
+    // an array of devices connected to this topology
     JsonArray components;
 
+    /**
+     * construct the topology id and devices connected
+     * @param id the topology id
+     * @param components devices connected with the topology
+     */
     public Topology(String id,JsonArray components){
         this.id = id;
         this.components = components;
     }
 
+    /**
+     * get the id of the topology
+     * @return topology id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * set the id with a new one
+     * @param id new topology id
+     */
     public void setId(String id) {
         this.id = id;
     }
 
+    /**
+     * get the devices connected to this topology
+     * @return a JsonArray of the devices
+     */
     public JsonArray getComponents() {
         return components;
     }
 
+    /**
+     * set the devices connected to this topology with a new one
+     * @param components new JsonArray of connected devices
+     */
     public void setComponents(JsonArray components) {
         this.components = components;
     }
