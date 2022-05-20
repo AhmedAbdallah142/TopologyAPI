e6eb35e16bcdef6840ce1892dc8954f3f3018c63
AhmedAbdallah142 <ahmed.abdalla_142@yahoo.com>
2022-05-20 10:44:49 +0200

    java doc code documentation

--- src/main/java/API/IAPI.java
+++ src/main/java/API/IAPI.java
@@ -5,11 +5,51 @@ import model.Topology;
 
 import java.util.ArrayList;
 
+
+/**
+ * This API provides the functionality to access, manage and store device topologies.
+ * @author Ahmed Abdallah
+ */
 public interface IAPI {
+    /**
+     * This method read a json file
+     * and store the topology exist in the memory
+     * @param FileName the topology json file name.
+     * @return true if the process succeeded,false if any problem occurred.
+     */
     boolean readJson(String FileName);
+    /**
+     * This method write a topology json file
+     * the file named with the topology id
+     * the topology should be existed in memory
+     * @param TopologyID the topology id of the required topology to write to a file.
+     * @return true if the process succeeded,false if any problem occurred.
+     */
     boolean writeJson(String TopologyID);
+
+    /**
+     * This method get all topologies exist in memory
+     * @return Arraylist of topologies exist in memory.
+     */
     ArrayList<Topology> queryTopologies();
+    /**
+     * This method delete a topology from the memory
+     * @param TopologyID the id of the topology required to be deleted.
+     * @return the deleted topology.
+     */
     Topology deleteTopology(String TopologyID);
+    /**
+     * get all devices connected with a specific topology
+     * @param TopologyID the id of the topology ot get devices connected to it.
+     * @return JsonArray of the devices.
+     */
     JsonArray queryDevices(String TopologyID);
+
+    /**
+     * get all devices connected to a given netList node in a specific given topology.
+     * @param TopologyID the id of the given topology.
+     * @param NetListNodeID the id on the given netList
+     * @return JsonArray of the devices.
+     */
     JsonArray queryDevicesWithNetListNode(String TopologyID,String NetListNodeID);
 }
--- src/main/java/file/FileOperation.java
+++ src/main/java/file/FileOperation.java
@@ -10,16 +10,33 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
+/**
+ * this class responsible for all file operation in the project
+ * @author Ahmed Abdallah
+ */
 public class FileOperation {
+
+    /**
+     * read a file with a given path and parse it to a JsonObject
+     * @param filePath the path of the file to be read
+     * @return the jsonObject gotten from the file
+     * @throws IOException
+     * @throws ParseException
+     */
     public JsonObject readJsonFile(String filePath) throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
         FileReader f = new FileReader(filePath);
         return (JsonObject) jsonParser.parse(f);
     }
 
+    /**
+     * this method write a topology to a json file
+     * @param data the topology required to be written
+     * @return true if topology written successfully else return false
+     */
     public boolean writeJsonFile(Topology data) {
         try {
-            FileWriter f = new FileWriter(String.valueOf(data.getId()));
+            FileWriter f = new FileWriter(String.valueOf(data.getId()+".json"));
             f.write(new Gson().toJson(data));
             f.flush();
         } catch (IOException e) {
